772bcbf3f785e809b2ba9ec4433d6c35ac0acdb4
XD Hiro <71639427+Hiro4444@users.noreply.github.com>
2021-09-16 14:52:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload() {
+}
+function setup() {
+    canvas=createCanvas(324,324);
+    canvas.center();
+}
+function draw() {
+}
+function take_snapshot() {
+    save('myFilterImage.png')
+}
